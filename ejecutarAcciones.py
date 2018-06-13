40d3c448ec4c96d6347082d97bcd03d178e062f1
Guillermo Giménez <guichezero@gmail.com>
2018-06-13 04:46:45 -0300

    Create ejecutarAcciones.py

---
+++ ejecutarAcciones.py
@@ -0,0 +1,11 @@
+# Modulo de taringaApi
+import taringaApi
+
+api = taringaApi.TaringApi()
+api.logear("usuario", "clave")
+#FUNCIONES A EJECUTAR
+
+#Acá van las funciones que se pueden usar.
+
+#FIN DE FUNCIONES A EJECUTAR
+api.deslogear()
